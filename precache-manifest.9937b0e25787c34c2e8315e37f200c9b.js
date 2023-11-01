044a3d6538769b39990945cc53a1f87760424524
Pursottam6003 <rahulsah6003@gmail.com>
2023-11-01 18:08:23 +0530

    Updates

---
+++ precache-manifest.9937b0e25787c34c2e8315e37f200c9b.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9315a81bce648f562fe43059aeb8b6bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57c218029a6a6dbf680a",
+    "url": "/static/css/main.8eeddde9.chunk.css"
+  },
+  {
+    "revision": "f9e601b298f44e658950",
+    "url": "/static/js/2.751a5151.chunk.js"
+  },
+  {
+    "revision": "57c218029a6a6dbf680a",
+    "url": "/static/js/main.1462f1ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22b97b09215d61c2ec3708612b966442",
+    "url": "/static/media/about.22b97b09.svg"
+  },
+  {
+    "revision": "06235ad58e83d77d08ff309078e965ee",
+    "url": "/static/media/achievementIcon.06235ad5.svg"
+  },
+  {
+    "revision": "786bcfcfe26ce9d5ee0b787a925a50a0",
+    "url": "/static/media/addovedi.786bcfcf.jpeg"
+  },
+  {
+    "revision": "a624c675750489378a6856ef99127fe7",
+    "url": "/static/media/airavat.a624c675.jpg"
+  },
+  {
+    "revision": "4a883fbbcf2550d2ce8aeaf12f4ac4bc",
+    "url": "/static/media/apsn.4a883fbb.jpeg"
+  },
+  {
+    "revision": "4d063bd36ce13361555223de901e4118",
+    "url": "/static/media/atulyam.4d063bd3.png"
+  },
+  {
+    "revision": "ba988410e85ebb9ebbb0a0038d176e20",
+    "url": "/static/media/bakeWithLove.ba988410.jpg"
+  },
+  {
+    "revision": "ed5c6f7ebf8bacb6752c4047ba5e3c29",
+    "url": "/static/media/blogIcon.ed5c6f7e.svg"
+  },
+  {
+    "revision": "a2750b0470ea216c181e424fe4a30cec",
+    "url": "/static/media/book.a2750b04.png"
+  },
+  {
+    "revision": "f32b48ad56d6ade47a1a24d4fe46224e",
+    "url": "/static/media/bye.f32b48ad.svg"
+  },
+  {
+    "revision": "2af6453bb73cab4658b9903fa305df78",
+    "url": "/static/media/chat_analyzer.2af6453b.jpg"
+  },
+  {
+    "revision": "7887c80e8534a0d3e1a52f1b26f5d21c",
+    "url": "/static/media/codechef.7887c80e.svg"
+  },
+  {
+    "revision": "f19a9d3c64bee0afb4f728cf7585e156",
+    "url": "/static/media/codingIcon.f19a9d3c.svg"
+  },
+  {
+    "revision": "63b84c274519437e5e29382f9365e753",
+    "url": "/static/media/contact.63b84c27.svg"
+  },
+  {
+    "revision": "7c0adc7e365b1048b032853bf924e70c",
+    "url": "/static/media/daily-tasks.7c0adc7e.png"
+  },
+  {
+    "revision": "aed553e77ea7d1f99d88adbdee2377a8",
+    "url": "/static/media/designathon.aed553e7.jpg"
+  },
+  {
+    "revision": "f372c7be16678194a0711c844d3395f9",
+    "url": "/static/media/education.f372c7be.svg"
+  },
+  {
+    "revision": "9a2d1b6371a41246759c456f24be43d7",
+    "url": "/static/media/experience.9a2d1b63.svg"
+  },
+  {
+    "revision": "c81005936531cef7f2065f7b23c30785",
+    "url": "/static/media/experienceIcon.c8100593.svg"
+  },
+  {
+    "revision": "985910d6401f678b3952b3fc1506fa6f",
+    "url": "/static/media/facebook.985910d6.svg"
+  },
+  {
+    "revision": "296fdd929b58a8b4ebd6ef671e8dfb5b",
+    "url": "/static/media/gear.296fdd92.svg"
+  },
+  {
+    "revision": "d850db70b1d525d15435cc01601cda2f",
+    "url": "/static/media/gfg.d850db70.svg"
+  },
+  {
+    "revision": "fa0baef5626985e710d6557b42dfe755",
+    "url": "/static/media/ghss.fa0baef5.png"
+  },
+  {
+    "revision": "55955e2f73d0de0bb8d8236a439fff21",
+    "url": "/static/media/google_kickstart.55955e2f.jpg"
+  },
+  {
+    "revision": "607dab740551c7e8d0d6638d070643d9",
+    "url": "/static/media/hackerank.607dab74.svg"
+  },
+  {
+    "revision": "e2cb07793f28b67ad31693ec8b52ad36",
+    "url": "/static/media/home.e2cb0779.svg"
+  },
+  {
+    "revision": "3cf0247169a92fdb001cb0710e40e6a1",
+    "url": "/static/media/housePricePrediction.3cf02471.webp"
+  },
+  {
+    "revision": "1eb233b0d9800ef026e9b50498ca8524",
+    "url": "/static/media/instagram.1eb233b0.svg"
+  },
+  {
+    "revision": "668c9912d45b75a2047d79aaf94fd195",
+    "url": "/static/media/internships.668c9912.jpg"
+  },
+  {
+    "revision": "347059cb3b4ed674982a61162dcf0b94",
+    "url": "/static/media/iocl.347059cb.pdf"
+  },
+  {
+    "revision": "709676e89e853a2245e1a82bafa50f91",
+    "url": "/static/media/iocl.709676e8.jpeg"
+  },
+  {
+    "revision": "cd52a5aaa28af8a8b565ba616139b265",
+    "url": "/static/media/iocl_office.cd52a5aa.jpg"
+  },
+  {
+    "revision": "0c3db3c0a476732169da2a39eca49abc",
+    "url": "/static/media/leetcode.0c3db3c0.svg"
+  },
+  {
+    "revision": "43d98a0d64688a9a5d50dbfa03147182",
+    "url": "/static/media/link.43d98a0d.svg"
+  },
+  {
+    "revision": "80cbfac4ca93531f4bfcff53788be549",
+    "url": "/static/media/namaste.80cbfac4.webp"
+  },
+  {
+    "revision": "dd32705c1878efda194a36370105a1e3",
+    "url": "/static/media/nitap.dd32705c.png"
+  },
+  {
+    "revision": "5347d1217be4e1dc510a8cf419261e76",
+    "url": "/static/media/nodmcu.5347d121.png"
+  },
+  {
+    "revision": "941ad6d1c792275e7cfa2ee6e5f8afbc",
+    "url": "/static/media/offer_letter_rebert.941ad6d1.pdf"
+  },
+  {
+    "revision": "979241d91a7fcd024f413d50597c4e3f",
+    "url": "/static/media/openAI.979241d9.webp"
+  },
+  {
+    "revision": "4b3b156005d18544495a42eedf291100",
+    "url": "/static/media/programming.4b3b1560.jpg"
+  },
+  {
+    "revision": "69076b9df74696865f293b8a42a877f4",
+    "url": "/static/media/projectIcon.69076b9d.svg"
+  },
+  {
+    "revision": "5951d9a9f1bd56258fc65bc3aecc61f6",
+    "url": "/static/media/projects.5951d9a9.svg"
+  },
+  {
+    "revision": "262a52be1169546fb57c42831f44ae72",
+    "url": "/static/media/pursottam.262a52be.jpg"
+  },
+  {
+    "revision": "8c5013872fc842de80c748f7b0ef6a39",
+    "url": "/static/media/pursottam_sah.8c501387.jpeg"
+  },
+  {
+    "revision": "e80e9303e81c1e05fbd20d0e575a8830",
+    "url": "/static/media/startup.e80e9303.jpg"
+  },
+  {
+    "revision": "467e87923a783856c251ddff2d586773",
+    "url": "/static/media/technodaya.467e8792.jpg"
+  },
+  {
+    "revision": "3a35824213dd1635e2f189e2a1d39f72",
+    "url": "/static/media/welcome.3a358242.svg"
+  }
+]);
